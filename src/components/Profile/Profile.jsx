e0b494fb79b2932c603cd4fc0bae82648a8f2d33
Deker <dimon.bond.94@gmail.com>
2022-10-04 21:06:25 +0300

    component Profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,38 @@
-import css from './Profile.module.css'
+import css from './Profile.module.css';
+import PropTypes from 'prop-types';
 
-export const Profile = () => {
+export const Profile = ({ avatar, username, tag, location, stats }) => {
+  return (
+    <div class={css.profile}>
+      <div class="description">
+        <img src={avatar} alt={username} class="avatar" />
+        <p class="name">{username}</p>
+        <p class="tag">@{tag}</p>
+        <p class="location">{location}</p>
+      </div>
 
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.PropTypes = {
+    avatar: PropTypes.string,
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string,
+    location: PropTypes.string,
+    stats: PropTypes.objectOf(PropTypes.number)
 }
